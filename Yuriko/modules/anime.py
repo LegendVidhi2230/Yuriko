939f549f65966de62a12fb9cd8cf63f8bc48a331
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-01-20 22:58:01 +0530

    Update anime.py

--- Yuriko/modules/anime.py
+++ Yuriko/modules/anime.py
@@ -580,7 +580,7 @@ __help__ = """
 
 ✗ /quote - `send quotes randomly as text`
 
-*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!*
+*𓆩ι кησω𓆪_𓆩𓆩🖤✨𓆪𓆪*
  """
 
 ANIME_HANDLER = DisableAbleCommandHandler("anime", anime, run_async=True)
