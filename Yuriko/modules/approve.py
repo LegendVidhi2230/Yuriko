7df5e7db178506fdbda3eef4875ce14c10b604a0
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-01-20 22:59:58 +0530

    Update approve.py

--- Yuriko/modules/approve.py
+++ Yuriko/modules/approve.py
@@ -209,7 +209,7 @@ __help__ = """
 
 ✗ /unapproveall - `Unapprove `*ALL* `users in a chat. This cannot be undone.`
 
-*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!*
+*`𓆩ι кησω𓆪_𓆩𓆩🖤✨𓆪𓆪`*
 """
 
 APPROVE = DisableAbleCommandHandler("approve", approve, run_async=True)
